9f1da00b8f87ea4cd3e8c71915179734d1a51ed6
Tushar Saxena <tushar@email.com>
2019-09-15 19:39:03 -0700

    Import all files

---
+++ build/precache-manifest.2caae3e6a937ba818264aaf835d3c0e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "834826f740676e10219b22cf7f71e547",
+    "url": "/my-first-app/index.html"
+  },
+  {
+    "revision": "7e419e88a065d1eeee4b",
+    "url": "/my-first-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "d0406e17c462b783b71a",
+    "url": "/my-first-app/static/js/2.e435e23f.chunk.js"
+  },
+  {
+    "revision": "7e419e88a065d1eeee4b",
+    "url": "/my-first-app/static/js/main.bed4f492.chunk.js"
+  },
+  {
+    "revision": "ce57d732af5098ef7812",
+    "url": "/my-first-app/static/js/runtime~main.aaf811d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-first-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/components/Home.tsx
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+
+export class Home extends React.Component {
+    render() {
+        return <div>Hi</div>;
+    }
+}
