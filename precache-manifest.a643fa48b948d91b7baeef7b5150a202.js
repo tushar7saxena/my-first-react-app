19fe63c8ea8139ba47023493d2930d39155290ae
Tushar Saxena <tushar@email.com>
2019-09-15 19:39:31 -0700

    Updates

---
+++ precache-manifest.a643fa48b948d91b7baeef7b5150a202.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2ee5e99be1c2ed1acef50520899a11b",
+    "url": "/my-first-react-app/index.html"
+  },
+  {
+    "revision": "e8bdd46069cc7cc2fe35",
+    "url": "/my-first-react-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c3fb625c776663873a86",
+    "url": "/my-first-react-app/static/js/2.1db0d67f.chunk.js"
+  },
+  {
+    "revision": "e8bdd46069cc7cc2fe35",
+    "url": "/my-first-react-app/static/js/main.797a0365.chunk.js"
+  },
+  {
+    "revision": "d5b51aa27f05cb881646",
+    "url": "/my-first-react-app/static/js/runtime~main.26a1b0f5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-first-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
